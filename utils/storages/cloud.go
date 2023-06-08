aae2633bb5da82f0d0d71f44f618e7bac1d3d90a
dimasyudhana <dimas.yudhana@gmail.com>
2023-06-08 21:47:17 +0700

    update storage

--- utils/storages/cloud.go
+++ utils/storages/cloud.go
@@ -5,59 +5,55 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"mime/multipart"
 	"os"
 	"strings"
 	"time"
 
 	"cloud.google.com/go/storage"
-	"github.com/GroupProject2-Kelompok4/BE/app/config"
 	"github.com/google/uuid"
 )
 
+const (
+	projectID  = "be-alta-17" // GANTI DENGAN PROJECT ID ANDA
+	bucketName = "immersive"  // GANTI DENGAN NAMA BUCKET ANDA
+)
+
 type ClientUploader struct {
-	storageClient *storage.Client
-	projectID     string
-	bucketName    string
-	path          string
+	cl         *storage.Client
+	projectID  string
+	bucketName string
+	uploadPath string
 }
 
-func InitGCPClient() *storage.Client {
-	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", config.GCP_CREDENTIAL)
+var Uploader *ClientUploader
+
+func init() {
+	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "./credential.json") // GANTI DENGAN JALUR FILE KREDENSIAL JSON ANDA
 	client, err := storage.NewClient(context.Background())
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 
-	image, err := file.Open()
-	if err != nil {
-		return "", err
-	}
-	defer image.Close()
-
-	sgcp := ClientUploader{
-		storageClient: InitGCPClient(),
-		projectID:     config.GCP_PROJECTID,
-		bucketName:    config.GCP_BUCKETNAME,
-		path:          config.GCP_PATH,
+	Uploader = &ClientUploader{
+		cl:         client,
+		bucketName: bucketName,
+		projectID:  projectID,
+		uploadPath: "latihan/",
 	}
 
-	imageURL, err := sgcp.UploadFile(image, file.Filename)
-	if err != nil {
-		return "", err
-	}
-
-	return imageURL, nil
 }
 
-func (s *ClientUploader) UploadFile(file io.Reader, fileName string) (string, error) {
+// UploadFile uploads an object
+func (c *ClientUploader) UploadFile(file multipart.File, object string) (string, error) {
 	rand := uuid.New().String()
 	ctx := context.Background()
 
 	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
 	defer cancel()
 
-	wc := s.storageClient.Bucket(s.bucketName).Object(s.path + fileName + rand).NewWriter(ctx)
-
+	// Upload an object with storage.Writer.
+	wc := c.cl.Bucket(c.bucketName).Object(c.uploadPath + object + rand).NewWriter(ctx)
 	if _, err := io.Copy(wc, file); err != nil {
 		return "", fmt.Errorf("io.Copy: %v", err)
 	}
@@ -65,7 +61,7 @@ func (s *ClientUploader) UploadFile(file io.Reader, fileName string) (string, er
 		return "", fmt.Errorf("Writer.Close: %v", err)
 	}
 
-	escapedFileName := strings.ReplaceAll(fileName, " ", "%20")
-	fileURL := "https://storage.googleapis.com/" + s.bucketName + "/" + s.path + escapedFileName + rand
+	escapedObject := strings.ReplaceAll(object, " ", "%20")
+	fileURL := "https://storage.googleapis.com/" + c.bucketName + "/" + c.uploadPath + escapedObject + rand
 	return fileURL, nil
 }
