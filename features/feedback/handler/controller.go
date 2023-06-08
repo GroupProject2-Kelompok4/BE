03b1c40dff63e31cc9d30b2635c4b4c260c210a2
dimasyudhana <dimas.yudhana@gmail.com>
2023-06-08 21:41:47 +0700

    update storage

--- features/feedback/handler/controller.go
+++ features/feedback/handler/controller.go
@@ -37,17 +37,25 @@ func (fh *feedbackHandler) RegisterFeedbackMentee() echo.HandlerFunc {
 			return c.JSON(http.StatusBadRequest, helper.ResponseFormat(http.StatusBadRequest, "Bad request", nil, nil))
 		}
 
-		var imageURL string
-		file, err := c.FormFile("proof")
-		if err == nil {
-			imageURL, err = storages.UploadImage(c, file)
-			if err != nil {
-				return c.JSON(http.StatusBadRequest, helper.ResponseFormat(http.StatusBadRequest, "Failed to upload image", nil, nil))
-			}
-			request.Proof = imageURL
+		f, err := c.FormFile("proof")
+		if err != nil {
+			c.Logger().Error("bad request while receive formfile")
+			return c.JSON(http.StatusBadRequest, helper.ResponseFormat(http.StatusBadRequest, "Bad request", nil, nil))
 		}
 
-		request.Proof = imageURL
+		blobFile, err := f.Open()
+		if err != nil {
+			c.Logger().Error("bad request while open fomfile")
+			return c.JSON(http.StatusBadRequest, helper.ResponseFormat(http.StatusBadRequest, "Bad request", nil, nil))
+		}
+
+		fileURL, err := storages.Uploader.UploadFile(blobFile, f.Filename)
+		if err != nil {
+			c.Logger().Error("bad request while upload to storage")
+			return c.JSON(http.StatusBadRequest, helper.ResponseFormat(http.StatusBadRequest, "Bad request", nil, nil))
+		}
+
+		request.Proof = fileURL
 		result, err := fh.service.RegisterFeedbackMentee(RequestToCore(request), userId)
 		if err != nil {
 			if strings.Contains(err.Error(), "empty") {
@@ -81,19 +89,27 @@ func (fh *feedbackHandler) UpdateFeedbackMentee() echo.HandlerFunc {
 			return c.JSON(http.StatusBadRequest, helper.ResponseFormat(http.StatusBadRequest, "Bad request", nil, nil))
 		}
 
-		var imageURL string
-		file, errReq := c.FormFile("proof")
-		if errReq == nil {
-			imageURL, errReq = storages.UploadImage(c, file)
-			if errReq != nil {
-				return c.JSON(http.StatusBadRequest, helper.ResponseFormat(http.StatusBadRequest, "Failed to upload image", nil, nil))
-			}
-			request.Proof = &imageURL
+		f, err := c.FormFile("proof")
+		if err != nil {
+			c.Logger().Error("bad request while receive formfile")
+			return c.JSON(http.StatusBadRequest, helper.ResponseFormat(http.StatusBadRequest, "Bad request", nil, nil))
 		}
 
-		request.Proof = &imageURL
-		err := fh.service.UpdateFeedbackMentee(RequestToCore(&request), feedbackId, userId)
+		blobFile, err := f.Open()
 		if err != nil {
+			c.Logger().Error("bad request while open fomfile")
+			return c.JSON(http.StatusBadRequest, helper.ResponseFormat(http.StatusBadRequest, "Bad request", nil, nil))
+		}
+
+		fileURL, err := storages.Uploader.UploadFile(blobFile, f.Filename)
+		if err != nil {
+			c.Logger().Error("bad request while upload to storage")
+			return c.JSON(http.StatusBadRequest, helper.ResponseFormat(http.StatusBadRequest, "Bad request", nil, nil))
+		}
+
+		request.Proof = &fileURL
+		err1 := fh.service.UpdateFeedbackMentee(RequestToCore(&request), feedbackId, userId)
+		if err1 != nil {
 			if strings.Contains(err.Error(), "empty") {
 				return c.JSON(http.StatusBadRequest, helper.ResponseFormat(http.StatusBadRequest, "Bad request", nil, nil))
 			}
--- utils/storages/cloud.go
+++ utils/storages/cloud.go
@@ -2,72 +2,70 @@ package storages
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"log"
-	"mime/multipart"
+	"os"
 	"strings"
 	"time"
 
 	"cloud.google.com/go/storage"
 	"github.com/GroupProject2-Kelompok4/BE/app/config"
 	"github.com/google/uuid"
-	"github.com/labstack/echo/v4"
-	"google.golang.org/api/option"
 )
 
-type StorageGCPConfig struct {
-	GCPClient  *storage.Client
-	ProjectID  string
-	BucketName string
-	Path       string
+type ClientUploader struct {
+	storageClient *storage.Client
+	projectID     string
+	bucketName    string
+	path          string
 }
 
 func InitGCPClient() *storage.Client {
-	ctx := context.Background()
-	client, err := storage.NewClient(ctx, option.WithCredentialsFile(config.GCP_CREDENTIAL))
+	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", config.GCP_CREDENTIAL)
+	client, err := storage.NewClient(context.Background())
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to create client: %v", err)
 	}
-	return client
-}
 
-func UploadImage(c echo.Context, file *multipart.FileHeader) (string, error) {
-	if file == nil {
-		return "", nil
-	}
 	image, err := file.Open()
 	if err != nil {
 		return "", err
 	}
 	defer image.Close()
-	sgcp := StorageGCPConfig{
-		GCPClient:  InitGCPClient(),
-		ProjectID:  config.GCP_PROJECTID,
-		BucketName: config.GCP_BUCKETNAME,
-		Path:       config.GCP_PATH,
+
+	sgcp := ClientUploader{
+		storageClient: InitGCPClient(),
+		projectID:     config.GCP_PROJECTID,
+		bucketName:    config.GCP_BUCKETNAME,
+		path:          config.GCP_PATH,
 	}
 
 	imageURL, err := sgcp.UploadFile(image, file.Filename)
 	if err != nil {
 		return "", err
 	}
+
 	return imageURL, nil
 }
 
-func (s *StorageGCPConfig) UploadFile(file io.Reader, fileName string) (string, error) {
+func (s *ClientUploader) UploadFile(file io.Reader, fileName string) (string, error) {
 	rand := uuid.New().String()
+	ctx := context.Background()
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
+	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
 	defer cancel()
-	wc := s.GCPClient.Bucket(s.BucketName).Object(s.Path + fileName).NewWriter(ctx)
+
+	wc := s.storageClient.Bucket(s.bucketName).Object(s.path + fileName + rand).NewWriter(ctx)
+
 	if _, err := io.Copy(wc, file); err != nil {
-		return "", err
+		return "", fmt.Errorf("io.Copy: %v", err)
 	}
 	if err := wc.Close(); err != nil {
-		return "", err
+		return "", fmt.Errorf("Writer.Close: %v", err)
 	}
 
 	escapedFileName := strings.ReplaceAll(fileName, " ", "%20")
-	fileURL := "https://storage.googleapis.com/" + s.BucketName + "/" + s.Path + escapedFileName + rand
+	fileURL := "https://storage.googleapis.com/" + s.bucketName + "/" + s.path + escapedFileName + rand
 	return fileURL, nil
 }
